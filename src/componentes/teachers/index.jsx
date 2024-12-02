b168a139e9a2af8a58e2bfaabfc015f845b72e2e
Igor Moura <igor.sousamoura@outlook.com>
2024-12-02 12:19:11 -0300

    deploy

--- src/componentes/teachers/index.jsx
+++ src/componentes/teachers/index.jsx
@@ -2,23 +2,21 @@ import React from "react";
 import Card from "../card";
 import "./styles.css";
 
-import Kevin from "./assets/kevin1.jpg";
 import Professor1 from "./assets/kevin1.jpg";
 import Professor2 from "./assets/teacher1.png";
 import Professor3 from "./assets/teacher2.png";
-
 export default function Teachers() {
     const teachers = [
         {
             name: "Jhonatan Dionizio",
             description: "Responsável pela parte de formação física dos atletas, com um foco inicial em adultos. Ele traz uma vasta experiência em condicionamento físico, ajudando os atletas a melhorar sua força, resistência e flexibilidade. Gustavo trabalha de perto com os judocas para garantir que estejam em excelente forma física.",
-            photo: Professor1,
+            photo: Professor3,
             appearance: "um homem de pele clara, cabelo curto e olhos castanhos",
         },
         {
             name: "Kevin Peters",
             description: "Seu foco está na formação de atletas mirins, promovendo o desenvolvimento técnico e físico desde uma idade jovem. Kevin é conhecido por sua abordagem pedagógica, criando um ambiente de aprendizado divertido e disciplinado. Tendo como objetivo, tornar seus jovens alunos a serem campeões e cidadãos exemplares.",
-            photo: Kevin,
+            photo: Professor1,
             appearance: "um homem jovem de pele clara, cabelo castanho loiro, vestindo um kimono de judo olhando para frente",
         },
         {
